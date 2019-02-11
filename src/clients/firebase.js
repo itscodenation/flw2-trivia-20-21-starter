78c2aed42febba8f6a68703a0c522c429b706b73
MLGpepe01 <sliva@citypolyhs.org>
2019-02-11 16:53:31 -0500

    Merge fixes

--- src/clients/firebase.js
+++ src/clients/firebase.js
@@ -1,17 +1,6 @@
 import firebase from "firebase";
 
 export function buildFirebase() {
-<<<<<<< HEAD
-  const app = firebase.initializeApp({
-    apiKey: "AIzaSyADAYC7lX5QVEspv8BUeV2uDqrFle8yQpk",
-    authDomain: "studio-trivia-db.firebaseapp.com",
-    databaseURL: "https://studio-trivia-db.firebaseio.com",
-    projectId: "studio-trivia-db",
-    storageBucket: "studio-trivia-db.appspot.com",
-    messagingSenderId: "736024037811"
-  });
-  return firebase.database(app);
-=======
   if (firebase.apps.length === 0) {
     const app = firebase.initializeApp({
       apiKey: "AIzaSyADAYC7lX5QVEspv8BUeV2uDqrFle8yQpk",
@@ -25,7 +14,6 @@ export function buildFirebase() {
   } else {
     return firebase.apps[0].database();
   }
->>>>>>> 5bb4ef389e2387e6ce3c9852128d2cd7dc874502
 }
 
 export function getRandomQuestion(questions) {
