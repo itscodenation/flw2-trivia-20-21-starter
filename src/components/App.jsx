20d516d2ea4642ba4c39e601c69c1995e06020a1
mburnett <mike.burnett@disneystreaming.com>
2019-10-31 17:17:08 -0400

    update deps after audit and some starter code tweaks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import '../css/App.css';
-// import components
+import Question from './Question';
+import { buildFirebase, getRandomQuestion } from '../clients/firebase';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      question: null
+    }
+  }
+
+  componentDidMount() {
+    this.db = buildFirebase();
+
+    this.db.ref('/questions').on('value', (snapshot)=> {
+      this.questions = snapshot.val();
+      let randomQuestion = this.getNewQuestion(this.questions);
+      console.log(this.questions, randomQuestion);
+      this.setState({
+          question: randomQuestion
+      })
+    });
+
+    this.getNewQuestion = this.getNewQuestion.bind(this);
+  }
+
+  getNewQuestion(questions) {
+    return getRandomQuestion(questions);
+  }
 
   render() {
     return (
       <div className="app">
-        Trivia!
-        
+        <h1>Trivia!</h1>
+        { this.state.question &&
+          this.state.question.question_text
+        }
       </div>
     );
   }
