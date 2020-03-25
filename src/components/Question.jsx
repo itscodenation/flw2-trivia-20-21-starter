eafa9203808b4b6ce99ffd336902bee002829524
Zoe Millard <zmillard@stevens.edu>
2020-03-25 18:29:51 -0400

    collab

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,22 +2,41 @@ import React, { Component } from 'react';
 // import components
 import {buildFirebase} from '../clients/firebase.js';
 
-
-
+import questions from '../sample_data.js';
+// var correct = false;
 
 class Question extends Component {
-
   
+  isCorrect = (index, correctIndex) => {
+    if(index === correctIndex){
+      // document.getElementById("result").innerHTML = "You are correct";
+      // alert("Correct")
+    }else{
+      // document.getElementById("result").innerHTML = "Try again";
+    };
+  };
+
   render() {
     var database = buildFirebase();
     var databaseRef = database.ref("/questions");
     databaseRef.once("value").then(function(data) {
-    const questions = data.val();
+    // const questions = data.val();
   // Do something with the questions  
     });
+
+    
     return (
       <div>
-        Add your QuestionText, Reset Button and AnswerButtons here.  
+        {questions.map(question => (
+          <div>
+            <h1>{question.Question}</h1>
+            <p id="result"></p>
+            <button onClick={(e) => (0 === question.correctAnswerIndex ? alert("Correct") : alert("Try Again"))}>{question.Answer[0]}</button>
+            <button onClick={(e) => (1 === question.correctAnswerIndex ? alert("Correct") : alert("Try Again"))}>{question.Answer[1]}</button>
+            <button onClick={(e) => (2 === question.correctAnswerIndex ? alert("Correct") : alert("Try Again"))}>{question.Answer[2]}</button>
+            <button onClick={(e) => (3 === question.correctAnswerIndex ? alert("Correct") : alert("Try Again"))}>{question.Answer[3]}</button> 
+          </div>
+        ))}
       </div>
     );
   }
---
+++ src/sample_data.js
@@ -0,0 +1,29 @@
+const questions = [ 
+    {
+        "Question": "What age did Emmett Till die?",
+        "Answer": [
+            "10", "9", "5", "14"
+        ],
+        "correctAnswerIndex": 3,
+    },{
+        "Question": "Solve for y. 34y-2x=12x-y",
+        "Answer": [
+            "14", "42", "3x/7", "2x/5"
+        ],
+        "correctAnswerIndex": 3,
+    },{
+        "Question": "What's the formula for centripetal force?",
+        "Answer": [
+            "y=mx+b", "y=x", "f=(mv^2)/r", "f=ma"
+        ],
+        "correctAnswerIndex": 2,
+    },{
+        "Question": "Who created The Crucible?",
+        "Answer": [
+            "Arthur Miller", "John Proctor", "James Dougherty", "Rebecca Miller"
+        ],
+        "correctAnswerIndex": 0,
+    }
+];
+
+export default questions;
