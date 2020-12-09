ecf629e7efa587b8c8d047bb1b21d12c99187ddc
Emily Garvey <41160828+emilygarvey@users.noreply.github.com>
2020-12-08 19:28:42 -0500

    Delete Question.jsx

--- src/components/Question.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import components
-
-function Question() {
-  return <div>Add your QuestionText, Reset Button and AnswerButtons here.</div>;
-}
-
-export default Question;
