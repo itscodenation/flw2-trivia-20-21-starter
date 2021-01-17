427804916ad1c01707c74f7076683f46ca90fb1c
Faran Jessani <faran@jessani.com>
2021-01-17 11:05:05 -0500

    Render a question from sample_data on the screen.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,17 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+let questionNumber = 0;
+
+function Question(props) {
+  return <div>{props.text}</div>
+}
+
 function App() {
-  return <div className="app">Trivia!</div>;
+  return <div className="app">
+    Trivia!
+    <Question text={data[questionNumber].question.text} />
+  </div>;
 }
 
 export default App;
