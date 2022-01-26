475cf63d7d0b8360a2f5de87a044d9bdd1279ffe
ElectricFocus <48684257+ElectricFocus@users.noreply.github.com>
2022-01-26 18:12:44 -0500

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,21 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  // const state, setState = useState()
+  const [questionNumber, setQuestionNumber] = useState(0);
+  return (
+    <div className="app">
+      Trivia!
+      <Question />
+      <Question text={data[questionNumber].question.text} />
+      <Question text={data[1].question.text} />
+      {/* {console.log(data[0])} */}
+    </div>
+  );
+}
+
+function Question(props) {
+  return <div>{props.text}</div>;
 }
 
 export default App;
